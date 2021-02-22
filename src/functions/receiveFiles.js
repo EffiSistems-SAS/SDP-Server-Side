a012f88f9651dcb108ab38e448860ba063685b5f
Miguel Rico <miguel35697@hotmail.com>
2021-02-22 12:30:02 -0500

    Refactors

---
+++ src/functions/receiveFiles.js
@@ -0,0 +1,13 @@
+const multer = require("multer");
+
+module.exports = (folderName = "uploads") => {
+  let upload = multer({
+    storage: multer.diskStorage({
+      destination: folderName,
+      filename: (req, file, save) => {
+        save(null, `${file.originalname}`);
+      },
+    }),
+  }).single("file");
+  return upload;
+};
