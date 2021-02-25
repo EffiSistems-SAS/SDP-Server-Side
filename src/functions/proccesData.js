0ec07c930da645ad3bad053ae6cf230dc1f505b9
spaezsuarez <spaezsuarez@gmail.com>
2021-02-25 15:52:15 -0500

    Condiciones nombres de archivos

--- src/functions/proccesData.js
+++ src/functions/proccesData.js
@@ -1,10 +1,16 @@
 const formatString = (input) => {
-    let texto = input.replaceAll('+',' ');
-    texto = texto.replaceAll('á','?');
-    texto = texto.replaceAll('é','?');
-    texto = texto.replaceAll('í','?');
-    texto = texto.replaceAll('ó','?');
-    texto = texto.replaceAll('ú','?');
+    //let texto = input.replace(/'+'/, " ");
+    let texto = input;
+    texto = texto.replace(/'á'/, "?");
+    texto = texto.replace(/'é'/, "?");
+    texto = texto.replace(/'í'/, "?");
+    texto = texto.replace(/'ó'/, "?");
+    texto = texto.replace(/'ú'/, "?");
+    texto = texto.replace(/'Á'/, "?");
+    texto = texto.replace(/'É'/, "?");
+    texto = texto.replace(/'Í'/, "?");
+    texto = texto.replace(/'Ó'/, "?");
+    texto = texto.replace(/'Ú'/, "?");
     return texto;
 }
 
